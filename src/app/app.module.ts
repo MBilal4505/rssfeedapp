b47a1b00ed3d0f18f2070727377b94bc76ef1d3f
Muhammad Bilal <bilalintl916@gmail.com>
2017-12-25 18:45:04 +0500

    rss feed app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+// Material Design modules by Google material.angular.io/
+// import { MdButtonModule } from '@angular2-material/button';
+// import { MdCardModule } from '@angular2-material/card';
+// import { MdCoreModule } from '@angular2-material/core';
+// import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+// import { MdToolbarModule } from '@angular2-material/toolbar';
+// import { MdSpinner } from '@angular2-material/progress-circle';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AppComponent } from './app.component';
+import { FeedService } from './feed.service';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { StripHtmlTagsPipe } from './pipe/strip-html-tags.pipe';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
 
+const appRoutes : Routes  = [
+{path:'', component:DashboardComponent},
+{path:'login', component:LoginComponent},
+{path:'register', component:RegisterComponent},
+{path:'profile', component:ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent,
+    StripHtmlTagsPipe,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ProfileComponent
+    // MdSpinner
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    FlashMessagesModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
+    // MdButtonModule,
+    // MdCardModule,
+    // MdCoreModule,
+    // MdIconModule,
+    // MdToolbarModule
   ],
-  providers: [],
+  providers: [
+  FeedService,
+  ValidateService,
+  AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
-import { FeedService } from './feed.service';
-import { FeedEntry } from './model/feed-entry';
+import { FeedService } from '../feed.service';
+import { FeedEntry } from '../model/feed-entry';
 // Add the RxJS Observable operators we need in this app.
 import './rxjs-operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class AppComponent implements OnInit {
-  /*private feedUrl: string = 'http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro';*/
+export class DashboardComponent implements OnInit {
+
+ /*private feedUrl: string = 'http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro';*/
   // if necessary, process the required link on this page:
   // https://rss2json.com and use the rss_url
   /*private feedUrl: string = 'http%3A%2F%2Fwww.20min.ch%2Frss%2Frss.tmpl%3Ftype%3Dchannel%26get%3D20%26lang%3Dro';*/
   private feedUrl: string = 'https://css-tricks.com/feed/';
   /*private feedUrl: string = 'https://www.becompany.ch/en/blog/feed.xml';*/
-  /*private feedUrl: string = 'https://techcrunch.com/feed/';*/
+  //private feedUrl: string = 'https://techcrunch.com/feed/';
   /*private feedUrl: string = 'http://www.rockclimbing.com/rss/routes/ascents/new.xml';*/
 
   private feeds: Array<FeedEntry> = [];
@@ -27,7 +27,8 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.refreshFeed();
+    var item = this.refreshFeed();
+    console.log('item',item);
   }
 
   private refreshFeed() {
@@ -38,5 +39,7 @@ export class AppComponent implements OnInit {
                       feed => this.feeds = feed.items,
                       error => console.log(error)
                     );
+  
   }
+
 }
